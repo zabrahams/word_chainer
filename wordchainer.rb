8478d150fbfc2e47e38fce6f614a67d392c0134f
Zach Abrahams <zachabrahams@gmail.com>
2014-11-16 20:39:53 -0500

    load dictionary and find adjacent words

--- wordchainer.rb
+++ wordchainer.rb
@@ -1,3 +1,27 @@
+require 'set'
+
 class WordChainer
+
+  LETTERS = ("a".."z").to_a
+
+  def initialize(dictionary_file_name)
+    @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
+    @dictionary = @dictionary.to_set
+  end
+
+  def adjacent_words(word)
+    adjacent_words = []
+
+    word.length.times do |i|
+      LETTERS.each do |letter|
+        next if word[i] == letter
+
+        poss_word = word.dup
+        poss_word[i] = letter
+        adjacent_words << poss_word if @dictionary.include?(poss_word)
+      end
+    end
+    adjacent_words
+  end
 
 end
